b55dbec8694a4f9de7804643ea6e2b4eae5bd20d
Venron Balisi <venron.balisi@gmail.com>
2017-04-26 17:57:12 -0700

    player death animation.

---
+++ MillennialPirate_v0.2/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    private Player playerScript;
+
+    public Slider hp;
+
+    public int maxHealth = 3;
+    public static int currentHealth = 3;
+
+	// Use this for initialization
+	void Start () {
+        playerScript = GameObject.Find("GameController").GetComponent<Player>();
+
+        hp = GameObject.Find("HP_2").GetComponent<Slider>();
+
+        hp.maxValue = maxHealth;
+        hp.value = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hp.value = currentHealth;
+
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            currentHealth--;
+        }
+
+        if(currentHealth <= 0)
+        {
+            StartCoroutine(Death());
+        }
+	}
+
+    IEnumerator Death ()
+    {
+        playerScript.playerAnim.SetBool("Idle", false);
+        playerScript.playerAnim.SetBool("Dead", true);
+
+        yield return new WaitForSeconds(5.0f);
+
+        HUDSet.Inst.EndGameCondition("Lose");
+
+    }
+}
--- MillennialPirate_v0.2/Assets/Scripts/UI/HUDSet.cs
+++ MillennialPirate_v0.2/Assets/Scripts/UI/HUDSet.cs
@@ -2,15 +2,9 @@
 using UnityEngine;
 
 public class HUDSet : Set {
-/*
-<<<<<<< HEAD
-    [SerializeField] public GameObject HealthBarsAnchor;
 
-    private void Start()
-    {
 
-    } 
-=======
+
     public static HUDSet Inst {
         get { return m_inst; }
     } static HUDSet m_inst;
@@ -76,5 +70,4 @@ public class HUDSet : Set {
 
         Pausing();
     }
->>>>>>> 8d96d5c4789c647d889c9956a55bcb0f2af6094d*/
 }
